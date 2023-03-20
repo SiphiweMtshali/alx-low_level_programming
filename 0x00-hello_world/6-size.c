936a16186ea763265dea112ddad5916c8557dae1
SiphiweMtshali <spmmtshali@gmail.com>
2023-03-20 07:15:03 -0700

    Updated script that fixes space issue

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+
 /**
- * main - Entry point 
+ * main - Entry point
  *
- * Return: Always  0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)\n");
-        printf("Size of an in: 4 buyte(s)\n");
-	printf("Size of a long int: 8 byte(s)\n");
-	printf("Size of a long long int: 8 byte(s)\n");
-	printf "Size of a float: 4 byte(s)\n);
-	return (0);   
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
+
